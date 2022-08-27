9be2f5c31c8f3208b217d0292c60e991f46b032c
Diana Valladares <96541076+dee-ah-nuh@users.noreply.github.com>
2022-08-27 08:36:09 -0500

    comiit

--- billingLeadsConversion.py
+++ billingLeadsConversion.py
@@ -15,22 +15,33 @@ import gspread
 import df2gspread as d2g
 
 
-# def gsheet_to_df(book,sheet,gspread):
-#     wb=gspread.open(book)
-#     ws=wb.worksheet(sheet)
-#     data=ws.get_all_values()
-#     headers=data.pop(0)
-#     df=pd.DataFrame(data,columns=headers)
-#     return df
 
-# sa = gspread.service_account(filename='universitymodelssheetscreds.json')
-# sheet = sa.open("Leads Billing Conversion")
-# idgs = '1bXJtdTW9Dqz02MILbaedaEv8AUQ89hKUoW_gO1xOGEw'
-# leadsandGrits = gsheet_to_df("Leads Billing Conversion", "Leads&Grits", sa)
-# leadsandGrits.fillna(0)
-# worksheet = sheet.worksheet("Leads&Grits")
-# print("Rows:", worksheet.row_count)
-# print("Columns:", worksheet.col_count)
+# set up credentials via cloud console
+#save file as json in same directory
+#import sheets to df module from UniversityModels
+#make google sheet a df
+#apply EDA
 
 
-billingdf= pd.read_csv("C:/Users/BEEMO/Downloads/Operation Graduate leads 2022-08-22 23-34 (1).csv")
+def gsheet_to_df(book,sheet,gspread):
+    wb=gspread.open(book)
+    ws=wb.worksheet(sheet)
+    data=ws.get_all_values()
+    headers=data.pop(0)
+    df=pd.DataFrame(data,columns=headers)
+    return df
+
+sa = gspread.service_account(filename='universitymodelssheetscreds.json')
+sheet = sa.open("Leads Billing Conversion")
+idgs = '1bXJtdTW9Dqz02MILbaedaEv8AUQ89hKUoW_gO1xOGEw'
+leadsandGrits = gsheet_to_df("Leads Billing Conversion", "Data", sa)
+leadsandGrits.fillna(0)
+worksheet = sheet.worksheet("Data")
+
+print("Rows:", worksheet.row_count)
+print("Columns:", worksheet.col_count)
+
+#119 columns, 1514 rows // 922 were DegreeSearch
+df = leadsandGrits
+df.drop_duplicates(subset=['id', 'primary_contact_primary_phone'] , inplace=True)
+
